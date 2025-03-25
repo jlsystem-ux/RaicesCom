f4ad2794d6c66a1ae4a928ba0d7e9a1c960f59ae
@laarboleda14 <laarboleda14@misena.edu.co>
2025-03-25 22:23:31 +0000

    Primer commit

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  alert("Gracias por tu mensaje. Nos pondremos en contacto pronto.");
+});
